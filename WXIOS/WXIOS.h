8c4c285590dd29fe84fc6084ff52dd6477c60c52
Pierluigi_MacBookPro <vasapolli.pierluigi@gmail.com>
2022-08-06 07:13:31 +0300

    Initial Commit

---
+++ WXIOS/WXIOS.h
@@ -0,0 +1,18 @@
+//
+//  WXIOS.h
+//  WXIOS
+//
+//  Created by iwaxT on 06/08/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WXIOS.
+FOUNDATION_EXPORT double WXIOSVersionNumber;
+
+//! Project version string for WXIOS.
+FOUNDATION_EXPORT const unsigned char WXIOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WXIOS/PublicHeader.h>
+
+
